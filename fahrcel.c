e2963e58897c929445b30ddbdc0d4eb6c160830a
Averoli <afanasenkavolha@gmail.com>
2023-07-19 16:22:30 +0200

    Changed to float

---
+++ fahrcel.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// print Fahrenheit-Celsius table
+// for fahr = 0, 20, ... 300
+int main()
+{
+    float farh, celsius;
+    int lower, upper, step;
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    farh = lower;
+    printf("Fahrenheit Celsius\n");
+    while (farh <= upper)
+    {
+        celsius = (5.0 / 9.0) * (farh - 32.0);
+        printf("%3.0f \t%6.1f\n", farh, celsius);
+        farh = farh + 20;
+    }
+}
