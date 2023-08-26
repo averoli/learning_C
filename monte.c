2a24f785875d34096630be6a01256cd4b0d9e43d
Averoli <afanasenkavolha@gmail.com>
2023-08-26 19:15:12 +0200

    Pointers basic

--- monte.c
+++ monte.c
@@ -5,19 +5,15 @@
 
 void suffle_cards(char *cards)
 {
-    int length = strlen(cards);
-    for (int i = length - 1; i > 0; i--)
-    {
-        int j = rand() % (i + 1); // Генерация случайного индекса от 0 до i
-        char temp = cards[i];
-        cards[i] = cards[j];
-        cards[j] = temp;
-    }
+    char card_0 = cards[0];
+    cards[0] = cards[1];
+    cards[1] = cards[2];
+    cards[2] = card_0;
 }
 
 int main()
 {
-    char cards[] = "JQK";
+    char *cards = "JQK";
     printf("You have 3 cards: %s\n", cards);
     suffle_cards(cards);
     printf("You have new 3 cards: %s\n", cards);
